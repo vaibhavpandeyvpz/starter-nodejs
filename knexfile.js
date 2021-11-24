2700b10da291c9c55b3d726def8e2609a8279159
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-11-24 12:21:14 +0530

    Include production database config.

--- knexfile.js
+++ knexfile.js
@@ -13,4 +13,8 @@ module.exports = {
     client: 'mysql',
     connection: process.env.DATABASE_URL,
   },
+  production: {
+    client: 'mysql',
+    connection: process.env.DATABASE_URL,
+  },
 };
