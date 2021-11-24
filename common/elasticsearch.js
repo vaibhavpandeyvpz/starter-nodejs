d90044f557554f68f6b55e227898a78e63f2b9a1
Vaibhav Pandey <contact@vaibhavpandey.com>
2021-11-24 12:24:38 +0530

    Use CommonJS style exports.

--- common/elasticsearch.js
+++ common/elasticsearch.js
@@ -1,3 +1,3 @@
 const { Client } = require('@elastic/elasticsearch');
 
-export default new Client({ node: process.env.ELASTICSEARCH_URL });
+module.exports = new Client({ node: process.env.ELASTICSEARCH_URL });
